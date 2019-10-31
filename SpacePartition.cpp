07ecbe896d31c551eba8fd19f783eee7eefd70b3
Aaron Curtis <hippopotamus.prime@cadae.net>
2019-10-30 23:46:45 -0400

    Clean up raw pointers in SpacePartition.
    Now using std::unique_ptr, std::variant to distinguish leaf vs. non-leaf
    nodes.

--- SpacePartition.cpp
+++ SpacePartition.cpp
@@ -35,16 +35,10 @@ class CompareMinEdge
 
 
 
-SpacePartition::SpacePartition(int _axis) : axis(_axis)
+SpacePartition::SpacePartition(int axis) : _axis(axis)
 {
 }
 
-SpacePartition::~SpacePartition()
-{
-    if(over)            delete over;
-    if(under)           delete under;
-    if(leaf_primitives) delete leaf_primitives;
-}
 
 void SpacePartition::BuildFromList(const vector<unique_ptr<Primitive>>& primitives)
 {
@@ -52,8 +46,7 @@ void SpacePartition::BuildFromList(const vector<unique_ptr<Primitive>>& primitiv
     PrimBox primbox;
 
     // Need to calculate a bounding box for the partition as a whole
-    BoundingBox total_box;
-    (*(primitives.begin()))->GetBoundingBox(total_box);
+    (*(primitives.begin()))->GetBoundingBox(_bounding_box);
 
     for (const auto& prim_ptr: primitives)
     {
@@ -61,74 +54,68 @@ void SpacePartition::BuildFromList(const vector<unique_ptr<Primitive>>& primitiv
         primbox.prim->GetBoundingBox(primbox.box);
         primboxes.push_back(primbox);
 
-        total_box.ExpandToFit(primbox.box);
+        _bounding_box.ExpandToFit(primbox.box);
     }
 
-    BuildFromList(primboxes, total_box);
+    BuildFromList(primboxes);
 }
 
 
-void SpacePartition::BuildFromList(const vector<PrimBox>& primitives,
-                                   const BoundingBox& box)
+void SpacePartition::BuildFromList(const vector<PrimBox>& primitives)
 {
     cout << "Space partition building from list with " << primitives.size()
          << " primitives." << endl;
 
-    bounding_box = box;
-
     BoundingBox over_box, under_box;
     bool need_to_split = FindPartitionPlane(primitives, over_box, under_box);
 
-    if(need_to_split)
+    if (need_to_split)
     {
         vector<PrimBox> over_primitives;
         vector<PrimBox> under_primitives;
 
         PrimBox primbox;
 
-        for(vector<PrimBox>::const_iterator it = primitives.begin();
-            it != primitives.end(); ++it)
+        for (const auto& primbox: primitives)
         {
             // TODO - using bounding boxes like this is not optimal.  Instead,
             // what we need to do is create a box for the SpacePartition and
             // see if the primitives intersect it.
 
-            primbox = *it;
-
-            if(IsOverBoundary(primbox))
+            if (IsOverBoundary(primbox))
             {
                 over_primitives.push_back(primbox);
             }
            
-            if(IsUnderBoundary(primbox))
+            if (IsUnderBoundary(primbox))
             {
                 under_primitives.push_back(primbox);
             }
         }
 
-        over = new SpacePartition((axis + 1) % 3);
-        under = new SpacePartition((axis + 1) % 3);
-        leaf_primitives = NULL;
-
-        over->BuildFromList(over_primitives, over_box);
-        under->BuildFromList(under_primitives, under_box);
+        NonLeafChildren children {
+            std::make_unique<SpacePartition>((_axis + 1) % 3),
+            std::make_unique<SpacePartition>((_axis + 1) % 3)};            
+        children.over->_bounding_box = over_box;
+        children.over->BuildFromList(over_primitives);
+        children.under->_bounding_box = under_box;
+        children.under->BuildFromList(under_primitives);
+        _children = std::move(children);
     }
     else
     {
-        over = NULL;
-        under = NULL;
-        leaf_primitives = new list<Primitive*>();
-
-        for(unsigned int i = 0; i < primitives.size(); ++i)
+        LeafChildren children;
+        for (const auto& primbox: primitives)
         {
-            leaf_primitives->push_back(primitives[i].prim);
+            children.primitives.push_back(primbox.prim);
         }
+        _children = std::move(children);
     }
 }
 
 
 bool SpacePartition::FindPartitionPlane(const vector<PrimBox>& primitives,
-                                        BoundingBox& over_box, BoundingBox& under_box)
+    BoundingBox& over_box, BoundingBox& under_box)
 {
     // For each primitive...
     //      Suppose we put the plane on the right edge?
@@ -249,36 +236,36 @@ bool SpacePartition::FindPartitionPlane(const vector<PrimBox>& primitives,
     // sets, since it allows the boxes to cross the splitting plane.  That's bad.
 
     int num_prims = primitives.size();
-    if(num_prims <= 3) return false;
+    if (num_prims <= 3) return false;
 
-    float lowest_cost = bounding_box.GetSurfaceArea() * num_prims * 0.99;
+    float lowest_cost = _bounding_box.GetSurfaceArea() * num_prims * 0.99;
     bool split = false;
     float current_cost;
 
     BoundingBox trial_over_box, trial_under_box;
 
-    for(int i = 0; i < num_prims; ++i)
+    for (const auto& primbox: primitives)
     {
-        current_cost = AttemptSplit(primitives, primitives[i].box.GetMaxValue(axis),
-                                    trial_over_box, trial_under_box);
+        current_cost = AttemptSplit(primitives,
+            primbox.box.GetMaxValue(_axis), trial_over_box, trial_under_box);
 
-        if(current_cost < lowest_cost)
+        if (current_cost < lowest_cost)
         {
             split = true;
             lowest_cost = current_cost;
-            splitting_plane = primitives[i].box.GetMaxValue(axis);
+            _splitting_plane = primbox.box.GetMaxValue(_axis);
             over_box = trial_over_box;
             under_box = trial_under_box;
         }
 
-        current_cost = AttemptSplit(primitives, primitives[i].box.GetMinValue(axis),
-                                    trial_over_box, trial_under_box);
+        current_cost = AttemptSplit(primitives,
+            primbox.box.GetMinValue(_axis), trial_over_box, trial_under_box);
 
-        if(current_cost < lowest_cost)
+        if (current_cost < lowest_cost)
         {
             split = true;
             lowest_cost = current_cost;
-            splitting_plane = primitives[i].box.GetMinValue(axis);
+            _splitting_plane = primbox.box.GetMinValue(_axis);
             over_box = trial_over_box;
             under_box = trial_under_box;
         }
@@ -289,48 +276,46 @@ bool SpacePartition::FindPartitionPlane(const vector<PrimBox>& primitives,
 
 
 
-float SpacePartition::AttemptSplit(const vector<PrimBox>& primitives, float trial_plane,
-                                   BoundingBox& over_box, BoundingBox& under_box)
+float SpacePartition::AttemptSplit(const vector<PrimBox>& primitives,
+    float trial_plane, BoundingBox& over_box, BoundingBox& under_box) const
 {
-    int num_prims = primitives.size();
-
     int over_count = 0;
     int under_count = 0;
 
     over_box.Zero();
     under_box.Zero();
 
-    for(int j = 0; j < num_prims; ++j)
+    for (const auto& primbox: primitives)
     {
         // See if we should add another primitive to the under-side set
 
-        if(primitives[j].box.GetMinValue(axis) <= trial_plane)
+        if (primbox.box.GetMinValue(_axis) <= trial_plane)
         {
             ++under_count;
 
-            if(under_count == 1)
+            if (under_count == 1)
             {
-                under_box = primitives[j].box;
+                under_box = primbox.box;
             }
             else
             {
-                under_box.ExpandToFit(primitives[j].box);
+                under_box.ExpandToFit(primbox.box);
             }
         }
 
         // See if we should add another primitive to the over-side set
 
-        if(primitives[j].box.GetMaxValue(axis) > trial_plane)
+        if (primbox.box.GetMaxValue(_axis) > trial_plane)
         {
             ++over_count;
 
-            if(over_count == 1)
+            if (over_count == 1)
             {
-                over_box = primitives[j].box;
+                over_box = primbox.box;
             }
             else
             {
-                over_box.ExpandToFit(primitives[j].box);
+                over_box.ExpandToFit(primbox.box);
             }
 
         }
@@ -346,26 +331,27 @@ float SpacePartition::AttemptSplit(const vector<PrimBox>& primitives, float tria
 
 
 
-bool SpacePartition::IsOverBoundary(const PrimBox& primbox)
+bool SpacePartition::IsOverBoundary(const PrimBox& primbox) const noexcept
 {
-    return (primbox.box.GetMaxValue(axis) > splitting_plane);
+    return (primbox.box.GetMaxValue(_axis) > _splitting_plane);
 }
 
-bool SpacePartition::IsUnderBoundary(const PrimBox& primbox)
+bool SpacePartition::IsUnderBoundary(const PrimBox& primbox) const noexcept
 {
-    return (primbox.box.GetMinValue(axis) <= splitting_plane);
+    return (primbox.box.GetMinValue(_axis) <= _splitting_plane);
 }
 
 
-size_t SpacePartition::GetSize()
+size_t SpacePartition::GetSize() const noexcept
 {
-    if(leaf_primitives)
+    if (std::holds_alternative<LeafChildren>(_children))
     {
-        return leaf_primitives->size();
+        return std::get<LeafChildren>(_children).primitives.size();
     }
-    else
+    else if (std::holds_alternative<NonLeafChildren>(_children))
     {
-        return over->GetSize() + under->GetSize();
+        const auto& non_leaf = std::get<NonLeafChildren>(_children);
+        return non_leaf.over->GetSize() + non_leaf.under->GetSize();
     }
 }
 
@@ -374,70 +360,72 @@ double SpacePartition::TraceRay(
     const Point& src, const Vector& ray, double hither,
     const Primitive*& object, Vector& normal) const
 {
+    // Does the ray hit the bounding box for this partition?
+    if (!_bounding_box.IntersectRay(src, ray, hither))
+        return -1.0;
+
     double result = -1.0;
 
-    // Does the ray hit the bounding box for this partition?
-
-    if (bounding_box.IntersectRay(src, ray, hither))
+    // If this is a leaf, intersect all the prmitives
+    if (std::holds_alternative<LeafChildren>(_children))
     {
-        // If this is a leaf, intersect all the prmitives
+        bool intersected = false;
+        double distance;
+        double min = -1;
+        Vector temp_normal;
 
-        if (leaf_primitives)
+        for (const auto& prim_ptr:
+                std::get<LeafChildren>(_children).primitives)
         {
-            bool intersected = false;
-            double distance;
-            double min = -1;
-            Vector temp_normal;
+            distance = prim_ptr->IntersectRay(src, ray, hither, temp_normal);
 
-            for (const auto& prim_ptr: *leaf_primitives)
+            if (distance >= hither)
             {
-                distance = prim_ptr->IntersectRay(src, ray, hither, temp_normal);
-
-                if (distance >= hither)
+                if (!intersected || (distance < min))
                 {
-                    if (!intersected || (distance < min))
-                    {
-                        intersected = true;
-                        min = distance;
-                        object = prim_ptr;
-                        normal = temp_normal;
-                    }
+                    intersected = true;
+                    min = distance;
+                    object = prim_ptr;
+                    normal = temp_normal;
                 }
             }
+        }
 
-            result = min;
+        result = min;
+    }
+    else
+    {
+        // Not a leaf node, so intersect the two sub-partitions.
+        // First one is chosen based on which side of the splitting
+        // plane the ray starts on.
+
+        const auto& non_leaf = std::get<NonLeafChildren>(_children);
+        if (src.GetValue(_axis) < _splitting_plane)
+        {
+            result = non_leaf.under->TraceRay(src, ray, hither, object, normal);
+            // Need to check the other side of the partition in two cases:
+            // - If the ray didn't hit anything, obviously
+            // - If the ray hit something that spans both halves and the
+            //   intersection is on the other side of the splitting plane;
+            //   in this case we can't say whether or not the found
+            //   intersection is actually the closest one
+
+            if ((result < hither)
+            ||  ((src + ray * result).GetValue(_axis) > _splitting_plane))
+            {
+                result = non_leaf.over->TraceRay(
+                    src, ray, hither, object, normal);
+            }
         }
         else
         {
-            // Not a leaf node, so intersect the two sub-partitions.
-            // First one is chosen based on which side of the splitting
-            // plane the ray starts on.
+            result = non_leaf.over->TraceRay(src, ray, hither, object, normal);
 
-            if (src.GetValue(axis) < splitting_plane)
+            if ((result < hither)
+            ||  ((src + ray * result).GetValue(_axis) < _splitting_plane))
             {
-                result = under->TraceRay(src, ray, hither, object, normal);
-                // Need to check the other side of the partition in two cases:
-                // - If the ray didn't hit anything, obviously
-                // - If the ray hit something that spans both halves and the
-                //   intersection is on the other side of the splitting plane;
-                //   in this case we can't say whether or not the found
-                //   intersection is actually the closest one
-
-                if ((result < hither)
-                ||  ((src + ray * result).GetValue(axis) > splitting_plane))
-                {
-                    result = over->TraceRay(src, ray, hither, object, normal);
-                }
-            }
-            else
-            {
-                result = over->TraceRay(src, ray, hither, object, normal);
-
-                if ((result < hither)
-                ||  ((src + ray * result).GetValue(axis) < splitting_plane))
-                {
-                    result = under->TraceRay(src, ray, hither, object, normal);
-                }
+                result = non_leaf.under->TraceRay(
+                    src, ray, hither, object, normal);
             }
         }
     }
--- SpacePartition.h
+++ SpacePartition.h
@@ -6,7 +6,7 @@
 
 #include <vector>
 #include <memory>
-#include <list>
+#include <variant>
 #include "Primitive.h"
 #include "BoundingBox.h"
 #include "VectorMath.h"
@@ -20,34 +20,42 @@ struct PrimBox
 class SpacePartition
 {
     public:
-        SpacePartition(int _axis = 0);
-        ~SpacePartition();
+        SpacePartition(int axis = 0);
 
         void BuildFromList(const std::vector<std::unique_ptr<Primitive>>& primitives);
         double TraceRay(const Point& src, const Vector& ray, double hither,
                         const Primitive*& object, Vector& normal) const;
 
-        size_t GetSize();
+        size_t GetSize() const noexcept;
 
     protected:
-        void BuildFromList(const std::vector<PrimBox>& primitives,
-                           const BoundingBox& box);
+        void BuildFromList(const std::vector<PrimBox>& primitives);
         bool FindPartitionPlane(const std::vector<PrimBox>& primitives,
-                                BoundingBox& over_box, BoundingBox& under_box);
-        float AttemptSplit(const std::vector<PrimBox>& primitives, float trial_plane,
-                           BoundingBox& over_box, BoundingBox& under_box);
-        bool IsOverBoundary(const PrimBox& primbox);
-        bool IsUnderBoundary(const PrimBox& primbox);
+            BoundingBox& over_box,
+            BoundingBox& under_box);
+        float AttemptSplit(const std::vector<PrimBox>& primitives,
+            float trial_plane,
+            BoundingBox& over_box,
+            BoundingBox& under_box) const;
+        bool IsOverBoundary(const PrimBox& primbox) const noexcept;
+        bool IsUnderBoundary(const PrimBox& primbox) const noexcept;
 
     private:
-        int axis;
-        float splitting_plane;
-        SpacePartition* over;
-        SpacePartition* under;
-        std::list<Primitive*>* leaf_primitives;
-        BoundingBox bounding_box;
+        struct NonLeafChildren
+        {
+            std::unique_ptr<SpacePartition> over;
+            std::unique_ptr<SpacePartition> under;
+        };
 
-        // TODO - make leaf_primitives a union
+        struct LeafChildren
+        {
+            std::vector<const Primitive*> primitives;
+        };
+
+        std::variant<NonLeafChildren, LeafChildren> _children;
+        BoundingBox _bounding_box;
+        float _splitting_plane;
+        int _axis;
 };
 
 
